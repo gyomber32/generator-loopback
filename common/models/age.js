ad17f87d0eacd21690f7ba01f32eb2cbd92e0b6d
Attila Gyömbér <gyomberattila@gmail.com>
2018-09-20 00:31:30 +0200

    Gender works perfectly, Age works, but need a little bit  more work on it

--- common/models/age.js
+++ common/models/age.js
@@ -1,41 +1,56 @@
 
-module.exports = function(Age) {
+module.exports = function (Age) {
 
-/**
- * getAge
- * @param {string} dob The age of the database. Values are given by using the dob (e.g.: 1995-03-12).
- * @callback {Function} callback Callback function
- * @param {Error|string} err Error object
- * @param {Age} result Result object
- */
-Age.getAge = function(dob, callback) {
+  /**
+   * getAge
+   * @param {string} dob The age of the database. Values are given by using the dob (e.g.: 1995-03-12).
+   * @callback {Function} callback Callback function
+   * @param {Error|string} err Error object
+   * @param {Age} result Result object
+   */
+  Age.getAge = function (dob, callback) {
 
-  // Replace the code below with your implementation.
-  // Please make sure the callback is invoked.
-  process.nextTick(function() {
-    var err = new Error('Not implemented');
-    callback(err);
-  });
-  
-}
+    var postgres = Age.app.dataSources.postgres.connector;
+    if (dob != undefined) {
+      var sql = 'SELECT dob FROM patients WHERE dob = ' + '$1' + ' LIMIT 2;';
+      var params = [dob];
+      postgres.execute(sql, params, (err, result) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(result);
+        }
+      });
+    } else {
+      var sql = 'SELECT dob FROM patients LIMIT 2;';
+      postgres.execute(sql, null, (err, result) => {
+        if (err) {
+          callback(err);
+        } else {
+          callback(result);
+        }
+      });
+    }
+  }
 
-
-
-
-Age.remoteMethod('getAge',
-  { isStatic: true,
-  produces: [ 'application/json' ],
-  accepts:
-   [ { arg: 'dob',
-       type: 'string',
-       description:
-        'The age of the database. Values are given by using the dob (e.g.: 1995-03-12).',
-       required: false,
-       http: { source: 'query' } } ],
-  returns:
-   [ { description: 'OK', type: [ 'Age' ], arg: 'data', root: true } ],
-  http: { verb: 'get', path: '' },
-  description: undefined }
-);
+  Age.remoteMethod('getAge',
+    {
+      isStatic: true,
+      produces: ['application/json'],
+      accepts:
+        [{
+          arg: 'dob',
+          type: 'string',
+          description:
+            'The age of the database. Values are given by using the dob (e.g.: 1995-03-12).',
+          required: false,
+          http: { source: 'query' }
+        }],
+      returns:
+        [{ description: 'OK', type: ['Age'], arg: 'data', root: true }],
+      http: { verb: 'get', path: '' },
+      description: undefined
+    }
+  );
 
 }
