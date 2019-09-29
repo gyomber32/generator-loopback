b7e9c5fcd72e34f13c4b2b9f5162e4fca869631a
ginger <gyomberattila@gmail.com>
2019-09-29 22:01:55 +0200

    Gender tests

---
+++ spec/gender.spec.js
@@ -0,0 +1,142 @@
+const Request = require("request");
+const GracefulShutdownManager = require('@moebius/http-graceful-shutdown').GracefulShutdownManager;
+
+describe("Server", () => {
+    var server;
+    beforeAll(() => {
+        server = require("../server/server");
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 300000;
+    });
+    afterAll(() => {
+        const shutdownManager = new GracefulShutdownManager(server);
+        process.on('SIGTERM', () => {
+            shutdownManager.terminate(() => {
+                console.log('Server is gracefully terminated');
+            });
+        });
+    });
+
+    describe("GET /getGender when all parameters are good (male case)", () => {
+        var data = {};
+        beforeAll((done) => {
+            Request.get("http://localhost:3100/api/Genders/getGender?gender=M&quantity=10", (error, response, body) => {
+                data.error = error;
+                data.status = response.statusCode;
+                data.body = JSON.parse(body);
+                done();
+            });
+        });
+        it("No error has to be occur", () => {
+            expect(data.error).toBeNull();
+        });
+        it("Status code has to be 200", () => {
+            expect(data.status).toBe(200);
+        });
+        it("Body has to contain appropriate data", () => {
+            expect(data.body).toContain({ "gender": "M" });
+        });
+        it("Body has not to contain inappropriate data", () => {
+            expect(data.body).not.toContain({ "gender": "F" });
+        });
+        it("Body has to contain appropriate number of data", () => {
+            expect(data.body.length).toEqual(10);
+        });
+    });
+
+    describe("GET /getGender when all parameters are good (female case)", () => {
+        var data = {};
+        beforeAll((done) => {
+            Request.get("http://localhost:3100/api/Genders/getGender?gender=F&quantity=10", (error, response, body) => {
+                data.error = error;
+                data.status = response.statusCode;
+                data.body = JSON.parse(body);
+                done();
+            });
+        });
+        it("No error has to be occur", () => {
+            expect(data.error).toBeNull();
+        });
+        it("Status code has to be 200", () => {
+            expect(data.status).toBe(200);
+        });
+        it("Body has to contain appropriate data", () => {
+            expect(data.body).toContain({ "gender": "F" });
+        });
+        it("Body has not to contain inappropriate data", () => {
+            expect(data.body).not.toContain({ "gender": "10" });
+        });
+        it("Body has to contain appropriate number of data", () => {
+            expect(data.body.length).toEqual(10);
+        });
+    });
+
+    describe("GET /getGender when quantity is the only parameter", () => {
+        var data = {};
+        beforeAll((done) => {
+            Request.get("http://localhost:3100/api/Genders/getGender?quantity=10", (error, response, body) => {
+                data.error = error;
+                data.status = response.statusCode;
+                data.body = JSON.parse(body);
+                done();
+            });
+        });
+        it("No error has to be occur", () => {
+            expect(data.error).toBeNull();
+        });
+        it("Status code has to be 200", () => {
+            expect(data.status).toBe(200);
+        });
+        it("Body has to contain appropriate data", () => {
+            expect(data.body).not.toBeNull();
+        });
+        it("Body has to contain appropriate number of data", () => {
+            expect(data.body.length).toEqual(10);
+        });
+    });
+
+    describe("GET /getGender when quantity has a wrong value", () => {
+        var data = {};
+        beforeAll((done) => {
+            Request.get("http://localhost:3100/api/Genders/getGender?quantity=-5", (error, response, body) => {
+                data.error = error;
+                data.status = response.statusCode;
+                data.body = JSON.parse(body);
+                done();
+            });
+        });
+        it("No error has to be occur", () => {
+            expect(data.error).toBeNull();
+        });
+        it("Status code has to be 500", () => {
+            expect(data.status).toBe(500);
+        });
+        it("Body has to have appropriate error message", () => {
+            expect(data.body.error.message).toEqual('LIMIT must not be negative');
+        });
+        it("Body has to contain empty array", () => {
+            expect(data.body.length).not.toEqual([]);
+        });
+    });
+
+    describe("GET /getGender when required parameter (quantity) is missing", () => {
+        var data = {};
+        beforeAll((done) => {
+            Request.get("http://localhost:3100/api/Genders/getGender?gender=M", (error, response, body) => {
+                data.error = error;
+                data.status = response;
+                data.body = JSON.parse(body);
+                done();
+            });
+        });
+        it("Error has to be occur", () => {
+            expect(data.body.hasOwnProperty('error')).toBe(true);
+        });
+        it("Status code has to be 400", () => {
+            expect(data.body.error.statusCode).toBe(400);
+        });
+        it("Body has to have appropriate error message", () => {
+            expect(data.body.error.message).toEqual('quantity is a required argument');
+        });
+    });
+
+});
