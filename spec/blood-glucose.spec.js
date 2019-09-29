3c33fd4d1cd7bcd88a93c914fdef7d9c96274fb4
ginger <gyomberattila@gmail.com>
2019-09-29 20:44:44 +0200

    Blood Glucose tested

--- spec/blood-glucose.spec.js
+++ spec/blood-glucose.spec.js
@@ -5,7 +5,7 @@ describe("Server", () => {
     var server;
     beforeAll(() => {
         server = require("../server/server");
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 200000;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL= 200000;
     });
     afterAll(() => {
         const shutdownManager = new GracefulShutdownManager(server);
@@ -16,10 +16,10 @@ describe("Server", () => {
         });
     });
 
-    describe("GET /getAge when all parameters are good", () => {
+    describe("GET /getBloodGlucose when all parameters are good", () => {
         var data = {};
         beforeAll((done) => {
-            Request.get("http://localhost:3100/api/Ages/getAge?age=50&quantity=10", (error, response, body) => {
+            Request.get("http://localhost:3100/api/BloodGlucoses/getBloodGlucose?bloodGlucose=7&quantity=10", (error, response, body) => {
                 data.error = error;
                 data.status = response.statusCode;
                 data.body = JSON.parse(body);
@@ -33,20 +33,20 @@ describe("Server", () => {
             expect(data.status).toBe(200);
         });
         it("Body has to contain appropriate data", () => {
-            expect(data.body).toContain({ "age": 50 });
+            expect(data.body).toContain({ "valuenum": 114 });
         });
         it("Body has not to contain inappropriate data", () => {
-            expect(data.body).not.toContain({ "age": 70 });
+            expect(data.body).not.toContain({ "valuenum": 150 });
         });
         it("Body has to contain appropriate number of data", () => {
             expect(data.body.length).toEqual(10);
         });
     });
 
-    describe("GET /getAge when quantity is the only parameter", () => {
+    describe("GET /getBloodGlucose when quantity is the only parameter", () => {
         var data = {};
         beforeAll((done) => {
-            Request.get("http://localhost:3100/api/Ages/getAge?quantity=10", (error, response, body) => {
+            Request.get("http://localhost:3100/api/BloodGlucoses/getBloodGlucose?quantity=10", (error, response, body) => {
                 data.error = error;
                 data.status = response.statusCode;
                 data.body = JSON.parse(body);
@@ -67,10 +67,10 @@ describe("Server", () => {
         });
     });
 
-    describe("GET /getAge when age has a wrong value", () => {
+    describe("GET /getBloodGlucose when bloodGlucose has a wrong value", () => {
         var data = {};
         beforeAll((done) => {
-            Request.get("http://localhost:3100/api/Ages/getAge?age=200&quantity=10", (error, response, body) => {
+            Request.get("http://localhost:3100/api/BloodGlucoses/getBloodGlucose?bloodGlucose=800&quantity=10", (error, response, body) => {
                 data.error = error;
                 data.status = response.statusCode;
                 data.body = JSON.parse(body);
@@ -83,15 +83,15 @@ describe("Server", () => {
         it("Status code has to be 200", () => {
             expect(data.status).toBe(200);
         });
-        it("Body has to contain empty array", () => {
+        it("Body has to be undefined", () => {
             expect(data.body).toEqual([]);
         });
     });
 
-    describe("GET /getAge when quantity has a wrong value", () => {
+    describe("GET /getBloodGlucose when quantity has a wrong value", () => {
         var data = {};
         beforeAll((done) => {
-            Request.get("http://localhost:3100/api/Ages/getAge?quantity=-5", (error, response, body) => {
+            Request.get("http://localhost:3100/api/BloodGlucoses/getBloodGlucose?quantity=-5", (error, response, body) => {
                 data.error = error;
                 data.status = response.statusCode;
                 data.body = JSON.parse(body);
@@ -112,18 +112,24 @@ describe("Server", () => {
         });
     });
 
-    describe("GET /getAge when required parameter (quantity) is missing", () => {
+    describe("GET /getBloodGlucose when required parameter (quantity) is missing", () => {
         var data = {};
         beforeAll((done) => {
-            Request.get("http://http://localhost:3100/api/Ages/getAge?age=50", (error, response, body) => {
+            Request.get("http://localhost:3100/api/BloodGlucoses/getBloodGlucose?bloodGlucose=7", (error, response, body) => {
                 data.error = error;
                 data.status = response;
-                data.body = body;
+                data.body = JSON.parse(body);
                 done();
             });
         });
         it("Error has to be occur", () => {
-            expect(data.error).not.toBeNull();
+            expect(data.body.hasOwnProperty('error')).toBe(true);
+        });
+        it("Status code has to be 400", () => {
+            expect(data.body.error.statusCode).toBe(400);
+        });
+        it("Body has to have appropriate error message", () => {
+            expect(data.body.error.message).toEqual('quantity is a required argument');
         });
     });
 
